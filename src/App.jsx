7835177f696ac15e76755bead4d50c4558d3543a
Christian Alvarenga <christianalvarenga201014@gmail.com>
2022-03-05 14:29:55 -0300

    Attempt number two to fix URL bug in production

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import AboutMeSection from "./components/AboutMeSection";
-import ExperienceSection from "./components/ExperienceSection";
-import ProjectsSection from "./components/ProjectsSection";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<AboutMeSection />} />
-        <Route path="experience" element={<ExperienceSection />} />
-        <Route path="projects" element={<ProjectsSection />} />
-      </Route>
+      <Route path="*" element={<Home />} />
+      <Route path="experience" element={<Home />} />
+      <Route path="projects" element={<Home />} />
     </Routes>
   );
 }
