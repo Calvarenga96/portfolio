d7be52871217adb3d3242382ceafc155aebc3c99
Christian Alvarenga <christianalvarenga201014@gmail.com>
2022-03-07 13:42:43 -0300

    I started to create the projects section

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,3 +1,58 @@
-const Project = () => {};
+import {
+  Badge,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+
+const Project = ({ name, link, alt, technologies, src }) => {
+  return (
+    <WrapItem w="40%">
+      <Flex direction="column" rowGap="15px">
+        <Link href={link} isExternal>
+          <Image
+            src={src}
+            alt={alt}
+            color="whiteColor"
+            borderRadius={5}
+            border="2px"
+            borderColor="purpleColor.300"
+            _hover={{ borderColor: "purpleColor.100" }}
+          />
+        </Link>
+        <Heading as="h3" color="whiteColor" size="md">
+          <Link href={link} isExternal>
+            {name}
+          </Link>
+        </Heading>
+        <Text color="whiteColor" fontWeight="600">
+          Technologies used
+        </Text>
+        <Wrap>
+          {technologies.map((technology) => {
+            return (
+              <WrapItem>
+                <Badge
+                  variant="subtle"
+                  key={technology}
+                  color="turquoiseColor.100"
+                  w="max-content"
+                  h="max-content"
+                  fontSize={10}
+                >
+                  {technology}
+                </Badge>
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+      </Flex>
+    </WrapItem>
+  );
+};
 
 export default Project;
--- src/components/ProjectsSection/index.jsx
+++ src/components/ProjectsSection/index.jsx
@@ -1,13 +1,37 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Wrap } from "@chakra-ui/react";
+import Calculator from "../../../assets/img/calculator.png";
+import Portfolio from "../../../assets/img/portfolio.png";
+import Project from "../Project";
 import Section from "../Section";
 
 const ProjectsSection = () => {
   return (
     <Box mt={[0, 0, 0]}>
       <Section title="Projects" />
-      <Heading color="whiteColor" fontSize="2xl" textAlign="center" py="30px">
-        Under Construction
-      </Heading>
+      <Wrap direction="row" justifyContent="center" mt={5}>
+        <Project
+          name="Portfolio"
+          src={Portfolio}
+          link="https://github.com/Calvarenga96/portfolio"
+          alt="My personal portfolio"
+          technologies={[
+            "React",
+            "Three.js",
+            "Chakra UI",
+            "React Router",
+            "React Helmet",
+            "Framer Motion",
+            "React Icons",
+          ]}
+        />
+        <Project
+          name="Splitter Calculator"
+          src={Calculator}
+          link="https://calvarenga96.github.io/splitter-calculator/"
+          alt="Calculator project"
+          technologies={["HTML5", "CSS3", "JavaScript", "Webpack"]}
+        />
+      </Wrap>
     </Box>
   );
 };
