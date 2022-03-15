54a3d7b84d267736ac5b7a042897e30e287cc88d
Christian Alvarenga <christianalvarenga201014@gmail.com>
2022-03-15 09:26:30 -0300

    I went back to the previous configuration of the router because with the current configuration, it does not go to any other section than the home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/experience" element={<Home />} />
-      <Route path="/projects" element={<Home />} />
-    </Routes>
+    <>
+      <Home />
+    </>
   );
 }
 
