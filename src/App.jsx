6d4e3c0535c926c10b30eddd609b21d7c9fbb2e9
Christian Alvarenga <christianalvarenga201014@gmail.com>
2022-03-14 22:50:49 -0300

    Attempt to fix production error 404 again

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/experience" element={<Home />} />
+      <Route path="/projects" element={<Home />} />
+    </Routes>
   );
 }
 
